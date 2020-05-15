1d9b32ab7d9d10c1fe49afedd6b4454eca23c6aa
deysourav738 <52924195+deysourav738@users.noreply.github.com>
2020-05-15 22:18:28 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@
 # # Multiclass-Text-Classification
 #     Author : Sourav Dey
 
-# In[1]:
+# In[155]:
 
 
 # Importing all the required module
@@ -13,42 +13,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 from sklearn.preprocessing import LabelEncoder
 from nltk.stem.porter import PorterStemmer
 import re
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
-from sklearn.neural_network import MLPClassifier
+from imblearn.combine import SMOTETomek
+from sklearn.linear_model import LogisticRegression
 
 
-# In[2]:
+# In[162]:
 
 
-data=pd.read_excel("Desktop/2749933_Entities.xlsx")
+def feature_processing(ini_features):
+    temp=re.sub('[^a-zA-Z]',' ',ini_features)
+    temp=temp.lower()
+    temp=temp.split()
+    temp=[ps.stem(word) for word in temp if not word in stopwords.words('english')]
+    return set(temp)
 
 
 # In[3]:
 
 
+def tfidf_get_scores(transformed_matrix,feature_names):
+    tf_scores={}
+    for i in range(transformed_matrix.shape[0]):
+        ft_index=transformed_matrix[i,:].nonzero()[1]
+        for j in ft_index:
+            if transformed_matrix[i,j] != 0:
+                tf_scores[feature_names[j]] = transformed_matrix[i,j]
+    for i in feature_names:
+        tf_scores[i]=tf_scores.pop(i)
+    return tf_scores
+
+
+# In[62]:
+
+
+def tfid_get_feature_values(y,tf_scores):
+    y_feat=[]
+    for i in tf_scores:
+        if i in y:
+            y_feat.append(tf_scores[i])
+        else:
+            y_feat.append(0)
+    return y_feat
+
+
+# In[6]:
+
+
+data=pd.read_excel("Desktop/2749933_Entities.xlsx")
+
+
+# In[7]:
+
+
 data.head()
 
 
-# In[4]:
+# In[8]:
 
 
 data.shape
 
 
-# In[5]:
+# In[9]:
 
 
 columns=list(data.columns)
 columns
 
 
-# In[6]:
+# In[10]:
 
 
 # Reshaping the data set for implementation
@@ -59,7 +99,7 @@ for j in columns:
 new_data
 
 
-# In[7]:
+# In[11]:
 
 
 # x are the features and y are target variables 
@@ -67,52 +107,58 @@ new_data=pd.DataFrame(new_data,columns=['x','y'])
 new_data
 
 
-# In[8]:
+# In[12]:
 
 
 new_data.isnull().sum()
 
 
-# In[9]:
+# In[13]:
 
 
 new_data=new_data.dropna() # Removing Null values
 
 
-# In[10]:
+# In[14]:
 
 
 new_data.groupby('y').count()
 
 
-# In[11]:
+# In[15]:
+
+
+smk = SMOTETomek(random_state=42)
+
+
+# In[16]:
 
 
 lb = LabelEncoder()
 new_data['y']=lb.fit_transform(new_data['y'])
 
 
-# In[12]:
+# In[17]:
 
 
 classes=lb.classes_
 classes
 
 
-# In[13]:
+# In[18]:
 
 
 new_data=new_data.reset_index()
 
 
-# In[14]:
+# In[130]:
 
 
 x=new_data['x']
 y=new_data['y']
 
 
-# In[15]:
+# In[131]:
 
 
 ps=PorterStemmer()
@@ -126,56 +172,181 @@ for i in range(0,len(x)):
     temp1.append(temp2)
 
 
-# In[16]:
+# In[132]:
 
 
-cv=CountVectorizer(min_df=1)
-x=cv.fit_transform(temp1)
+tf=TfidfVectorizer(max_features=400)
+x=tf.fit_transform(temp1)
 
 
-# In[17]:
-
-
-x=x.toarray()
-
-
-# In[18]:
+# In[133]:
 
 
 x
 
 
-# In[19]:
+# In[134]:
 
 
-ft=cv.get_feature_names()
+ft=tf.get_feature_names()
 
 
-# In[20]:
+# In[135]:
 
 
-ft
+tf_scores=tfidf_get_scores(x,ft)
 
 
-# In[21]:
+# In[136]:
+
+
+len(tf_scores)
+
+
+# In[137]:
 
 
 x.shape,y.shape
 
 
-# In[22]:
+# In[138]:
 
 
-nn=MLPClassifier(hidden_layer_sizes=200,random_state=40)
+x,y
+
+
+# In[139]:
+
+
+x=x.toarray()
+
+
+# In[140]:
+
+
+x,y=smk.fit_sample(x,y)
+
+
+# In[141]:
+
+
+x.shape,y.shape
+
+
+# In[142]:
+
+
+reg=LogisticRegression()
 print("Accuracy on training set:")
-res=cross_val_score(nn,x,y,cv=10,scoring="accuracy")
+res=cross_val_score(reg,x,y,cv=10,scoring="accuracy")
 print("Average accuracy:\t{0:}\n".format(np.mean(res)))
 print("Standard Deviation:\t{0:}\n".format(np.std(res)))
-nn.fit(x,y)
+reg.fit(x,y)
 
 
-# In[23]:
+# In[163]:
 
 
-pickle.dump(nn,open('model.pkl','wb'))
+pickle.dump(reg,open('model.pkl','wb'))
+
+
+# In[164]:
+
+
+pickle.dump(tf_scores,open('tf_scores.pkl','wb'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[91]:
+
+
+df=pd.read_excel("Desktop/2749936_mongodb_data.xls")
+
+
+# In[92]:
+
+
+df=df.dropna(axis=0)
+
+
+# In[93]:
+
+
+df=df.reset_index()
+
+
+# In[94]:
+
+
+ini=[df['Description'][i] for i in range(0,len(df))]
+
+
+# In[95]:
+
+
+fin=[feature_processing(ini[i]) for i in range(len(ini))]
+
+
+# In[143]:
+
+
+final=[tfid_get_feature_values(fin[i],tf_scores) for i in range(len(fin))]
+
+
+# In[144]:
+
+
+prediction=reg.predict(final)
+output=classes[prediction]
+
+
+# In[145]:
+
+
+prediction
+
+
+# In[146]:
+
+
+output
+
+
+# In[147]:
+
+
+unique,count=np.unique(output,return_counts=True)
+
+
+# In[152]:
+
+
+unique,count
+
+
+# In[154]:
+
+
+plt.bar(unique,count,color=['r','g','b','y'])
+plt.xticks(unique,unique,rotation=90)
+
+
+# In[ ]:
 
