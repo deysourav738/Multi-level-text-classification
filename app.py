613772bc5677f6bdec9a2cb5dfa7cbe9f92cd5ac
deysourav738 <52924195+deysourav738@users.noreply.github.com>
2020-05-15 22:16:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,52 +8,74 @@ from flask import Flask,request,render_template,url_for
 import numpy as np
 import pandas as pd
 import pickle
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import re
 
 
 # In[2]:
 
 
-df=pd.read_excel("Desktop/2749936_mongodb_data.xls")
+def feature_processing(ini_features):
+    ps=PorterStemmer()
+    temp=re.sub('[^a-zA-Z]',' ',ini_features)
+    temp=temp.lower()
+    temp=temp.split()
+    temp=[ps.stem(word) for word in temp if not word in stopwords.words('english')]
+    return set(temp)
 
 
 # In[3]:
 
 
-df=df.dropna(axis=0)
+def tfid_get_feature_values(y,tf_scores):
+    y_feat=[]
+    for i in tf_scores:
+        if i in y:
+            y_feat.append(tf_scores[i])
+        else:
+            y_feat.append(0)
+    return y_feat
 
 
 # In[4]:
 
 
-def final_feature(ini_features):
-    temp=re.sub('[^a-zA-Z]',' ',ini_features)
-    temp=temp.lower()
-    temp=temp.split()
-    temp=[ps.stem(word) for word in temp if not word in stopwords.words('english')]
-    temp=set(temp)
-    final_features=[]
-    for i in ft:
-        if i in temp:
-            val = 1
-        else:
-            val = 0
-        final_features.append(val)
-    return list(final_features)
+df=pd.read_excel("Desktop/2749936_mongodb_data.xls")
 
 
 # In[5]:
 
 
-app = Flask(__name__)
+df=df.dropna(axis=0)
 
 
 # In[6]:
 
 
-model=pickle.load(open('model.pkl','rb'))
+df=df.reset_index()
 
 
 # In[7]:
+
+
+app = Flask(__name__)
+
+
+# In[8]:
+
+
+model=pickle.load(open('model.pkl','rb'))
+tf_scores=pickle.load(open('tf_scores.pkl','rb'))
+
+
+# In[9]:
+
+
+classes=['Audio Video Technology', 'Computer Technology','Electrical Machinery Apparatus Energy', 'Telecommunication']
+
+
+# In[10]:
 
 
 @app.route('/')
@@ -61,29 +83,25 @@ def home():
     return render_template('index.html')
 
 
-# In[8]:
+# In[11]:
 
 
 @app.route('/predict',methods=['POST'])
 def predict():
     pun=request.form.values()
     features=list(df.loc[df['Publication number'] == pun]['Description'])[0]
-    prediction =final_feature(features)
-    output=classes[prediction]
+    fin_features=feature_processing(features)
+    fin_features=[tfid_get_feature_values(fin_features,tf_scores)]
+    prediction=model.predict(fin_features)
+    output=classes[prediction[0]]
     return render_template('index.html',prediction_text='This application belongs to {}'.format(output))
 
 
-# In[9]:
+# In[13]:
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
-
-
-# In[10]:
-
-
-df
+    app.run(debug = False)
 
 
 # In[ ]:
